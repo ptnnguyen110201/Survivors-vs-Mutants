65ee9839906a4a5ec36ab75039b603dd3ac91806
Ndd <duonghumg145@gmail.com>
2025-03-21 00:26:18 +0700

    add func generic in class loadComponent

--- Assets/_Data/_Script/PatternManager/LoadComPonent.cs
+++ Assets/_Data/_Script/PatternManager/LoadComPonent.cs
@@ -25,9 +25,42 @@ public abstract class LoadComPonent : MonoBehaviour
         this.ResetValue();
         this.LoadComponents();
     }
+    protected void LoadScript<T>(ref T script, GameObject obj) where T : class
+    {
+        if (script != null)
+            return;
+        script = obj.GetComponent<T>();
+        DebugLoadComponent(typeof(T).Name);
+    }
 
+    protected void LoadScript<T>(ref T script, Transform obj) where T : class
+    {
+        if (script != null)
+            return;
+        script = obj.GetComponent<T>();
+        DebugLoadComponent(typeof(T).Name);
+    }
 
+    protected void LoadScript<T>(ref T script) where T : class
+    {
+        if (script != null)
+            return;
+        script = gameObject.GetComponent<T>();
+        DebugLoadComponent(typeof(T).Name);
+    }
 
+    protected void LoadScriptInChild<T>(ref T script) where T : class
+    {
+        if (script != null)
+            return;
+        script = gameObject.GetComponentInChildren<T>();
+        DebugLoadComponent(typeof(T).Name);
+    }
+
+    private void DebugLoadComponent(string nameComponent)
+    {
+        Debug.LogWarning("Load Script " + nameComponent, gameObject);
+    }
 
 
 
